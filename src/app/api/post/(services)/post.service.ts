9e80c0f09e3e09337bc0c06f2f7fcfbd5fd18240
Srijan Bista <srijanbista4284@gmail.com>
2024-04-30 10:46:18 +0545

    added backend services

--- src/app/api/post/(services)/post.service.ts
+++ src/app/api/post/(services)/post.service.ts
@@ -1,5 +1,9 @@
 import { Post } from "@prisma/client";
 import { prisma } from "../../../../../prisma/db";
+import {
+  createAuthor,
+  getAuthorByName,
+} from "../../author/(services)/author.service";
 
 export async function getPosts() {
   try {
@@ -14,13 +18,25 @@ export async function getPosts() {
   }
 }
 
-export async function addPost(data: Post) {
+export async function addPost(postData: Post) {
   try {
+    let author = await getAuthorByName(postData.authorName);
+    if (!author) {
+      author = await createAuthor(postData.authorName);
+    }
     const post = await prisma.post.create({
-      data: data,
+      data: {
+        content: postData.content,
+        title: postData.title,
+        authorName: postData.authorName,
+        author: {
+          connect: { id: author?.id },
+        },
+      },
     });
     return post;
   } catch (error) {
     console.log(error);
+    throw error; // Re-throwing the error for handling at the caller's level
   }
 }
